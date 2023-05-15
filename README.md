82652563aef49b88e76dbacdea2081248b9f76e9
frantecbh <franciscomenezesbh@gmail.com>
2023-05-15 17:46:14 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+npm init - y
+
+npm i typescript @types/node tsx tsup -D
+
+npx tsc --init
+
+npm i fastify
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { app } from "./app";
+
+app.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333
+
+}).then(() => {
+  console.log('HTTP Server on running')
+}).catch((e) => {
+  console.error(e)
+  process.exit(1)
+})
